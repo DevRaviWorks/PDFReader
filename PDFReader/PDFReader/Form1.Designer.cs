721b4ab8b8563d3fc022cc5fcdb1992968700ecb
RAVI-PC\Ravi <pundir.ravi.yoyo@gmail.com>
2022-07-24 16:23:19 +0530

    Initial Commit for PDF Reader

---
+++ PDFReader/PDFReader/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿
+namespace PDFReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PDF_Reader = new AxAcroPDFLib.AxAcroPDF();
+            ((System.ComponentModel.ISupportInitialize)(this.PDF_Reader)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PDF_Reader
+            // 
+            this.PDF_Reader.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PDF_Reader.Enabled = true;
+            this.PDF_Reader.Location = new System.Drawing.Point(26, 12);
+            this.PDF_Reader.Name = "PDF_Reader";
+            this.PDF_Reader.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("PDF_Reader.OcxState")));
+            this.PDF_Reader.Size = new System.Drawing.Size(545, 327);
+            this.PDF_Reader.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PDF_Reader);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PDF_Reader)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxAcroPDFLib.AxAcroPDF PDF_Reader;
+    }
+}
+
